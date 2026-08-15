5e899e25c0b943677720563063a2591d76af8522
Edmilson Dantas <eddantad@gmail.com>
2026-08-15 15:40:09 -0300

    Front Controller com parser de rotas dinâmicas

--- public/index.php
+++ public/index.php
@@ -1,5 +1,4 @@
 <?php
-
 require_once __DIR__ . '/../app/Controllers/HomeController.php';
 require_once __DIR__ . '/../app/Controllers/UsuarioController.php';
 require_once __DIR__ . '/../app/Controllers/LivroController.php';
@@ -11,34 +10,30 @@ $rotas = require __DIR__ . '/../routes/web.php';
 
 $metodo = $_SERVER['REQUEST_METHOD'];
 $caminho = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$caminho = rtrim($caminho, '/') ?: '/';
 
-// Descobre o prefixo da subpasta onde o projeto está publicado
-// (ex.: /Projeto-Biblioteca-Entrega3/.../public) e remove esse
-// prefixo da URL, para que as rotas continuem sendo comparadas
-// apenas com "/", "/livros", etc, não importa em qual pasta do
-// htdocs o projeto esteja instalado.
-$baseDir = str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME']));
+function rotear(array $rotas, string $metodo, string $caminho): ?array
+{
+    foreach ($rotas[$metodo] ?? [] as $padrao => $acao) {
+        $regex = preg_replace('#\{[a-zA-Z_]+\}#', '([^/]+)', $padrao);
+        $regex = '#^' . $regex . '$#';
 
-if ($baseDir !== '/' && $baseDir !== '' && strpos($caminho, $baseDir) === 0) {
-    $caminho = substr($caminho, strlen($baseDir));
+        if (preg_match($regex, $caminho, $matches)) {
+            array_shift($matches); // remove o match completo
+            return ['acao' => $acao, 'parametros' => $matches];
+        }
+    }
+    return null;
 }
 
-$caminho = rtrim($caminho, '/');
+$rota = rotear($rotas, $metodo, $caminho);
 
-if ($caminho == '') {
-    $caminho = '/';
-}
-
-$rota = $rotas[$metodo][$caminho] ?? null;
-
-if (!$rota) {
+if ($rota === null) {
     http_response_code(404);
     require __DIR__ . '/../app/Views/errors/404.php';
     exit;
 }
 
-$controllerNome = $rota[0];
-$acao = $rota[1];
-
-$controller = new $controllerNome();
-$controller->$acao();
+[$controller, $metodoAcao] = $rota['acao'];
+$controller = new $controller();
+call_user_func_array([$controller, $metodoAcao], $rota['parametros']);
