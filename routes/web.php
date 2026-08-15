6418fe96050a44c437d9d6d3beb67670d1864122
Edmilson Dantas <eddantad@gmail.com>
2026-08-15 15:35:02 -0300

    Entrega Parcial 4: rotas dinâmicas de editar, atualizar e excluir livros

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,18 @@
 <?php
-
 return [
     'GET' => [
         '/' => ['HomeController', 'index'],
         '/usuarios' => ['UsuarioController', 'listar'],
         '/livros' => ['LivroController', 'listar'],
         '/livros/cadastrar' => ['LivroController', 'cadastrar'],
+        '/livros/editar/{id}' => ['LivroController', 'editar'],
         '/autores' => ['AutorController', 'listar'],
         '/categorias' => ['CategoriaController', 'listar'],
         '/emprestimos' => ['EmprestimoController', 'listar'],
     ],
     'POST' => [
         '/livros/cadastrar' => ['LivroController', 'salvar'],
+        '/livros/atualizar/{id}' => ['LivroController', 'atualizar'],
+        '/livros/excluir/{id}' => ['LivroController', 'excluir'],
     ],
 ];
